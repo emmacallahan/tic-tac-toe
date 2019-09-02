06f900edab62c7fe340a6bda3baab12da5f7c1e7
emmacallahan <emma.callahan@scout24.com>
2019-09-02 16:39:31 +0200

    initial commit--creates board and player prompts for tic tac toe game

---
+++ tictactoe.py
@@ -0,0 +1,10 @@
+print("Let's play Tic-Tac-Toe!")
+board_horizantal = '   1 2 3'
+board_vertical_A = '\n A _|_|_'
+board_vertical_B = '\n B _|_|_'
+board_vertical_C = '\n C  | | '
+print(board_horizantal, board_vertical_A, board_vertical_B, board_vertical_C)
+print('Player 1, please select a space:')
+print('Player 2, please select a space:')
+print('Player 1, your turn:')
+print('Player 2, your turn:')
