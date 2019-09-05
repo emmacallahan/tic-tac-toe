5d084cd84d7f533d5f73de0a3839b78bb177feb8
emmacallahan <emma.callahan@scout24.com>
2019-09-05 21:00:15 +0200

    WIP: Not behaving as expected--Player symbol is not switching

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,29 @@
 print("Let's play Tic-Tac-Toe!")
-board_horizantal = '   1 2 3'
-board_vertical_A = '\n A _|_|_'
-board_vertical_B = '\n B _|_|_'
-board_vertical_C = '\n C  | | '
-print(board_horizantal, board_vertical_A, board_vertical_B, board_vertical_C)
-print('Player 1, please select a space:')
-print('Player 2, please select a space:')
-print('Player 1, your turn:')
-print('Player 2, your turn:')
+board_columns = '   1 2 3'
+board_row_A = '\n A _|_|_'
+board_row_B = '\n B _|_|_'
+board_row_C = '\n C  | | '
+
+def print_board():
+    print(board_columns, board_row_A, board_row_B, board_row_C)
+
+print_board()
+player_symbol = 'O'
+def switch_player_symbol(player_symbol):
+    if player_symbol == 'O':
+        player_symbol == 'X'
+        return player_symbol
+    else:
+        return player_symbol
+
+
+
+print(player_symbol)
+
+print('Player '+player_symbol+', please select a space:')
+switch_player_symbol(player_symbol)
+print('Player '+player_symbol+', please select a space:')
+
+##def check_for_winning_combination():
+##    for char in board_vertical_A,board_vertical_B,board_vertical_C:
+##       if
