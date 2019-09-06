3ce8c1455f47e00cd44d4dd0cff0440011260203
emmacallahan <emma.callahan@scout24.com>
2019-09-06 16:34:58 +0200

    WIP: prints board and takes input, but will not print out new board with new input

--- tictactoe.py
+++ tictactoe.py
@@ -7,22 +7,32 @@ board_row_C = '\n C  | | '
 def print_board():
     print(board_columns, board_row_A, board_row_B, board_row_C)
 
+def place_player_choice():
+    player_input.lower()
+    if player_input == ['A1','1A']:
+        board_row_A = '\n A X|_|_'
+
 print_board()
-player_symbol = 'O'
-def switch_player_symbol(player_symbol):
-    if player_symbol == 'O':
-        player_symbol == 'X'
+
+player_symbol = 'X'
+player_input = ''
+
+def switch_player_symbol():
+    if player_symbol == 'X':
+        player_symbol == 'O'
         return player_symbol
     else:
         return player_symbol
 
+player_imput = input('Player '+player_symbol+', please select a space:')
 
+print(player_input)
 
-print(player_symbol)
-
-print('Player '+player_symbol+', please select a space:')
-switch_player_symbol(player_symbol)
-print('Player '+player_symbol+', please select a space:')
+switch_player_symbol()
+print(player_input)
+place_player_choice()
+print_board()
+print(player_input)
 
 ##def check_for_winning_combination():
 ##    for char in board_vertical_A,board_vertical_B,board_vertical_C:
