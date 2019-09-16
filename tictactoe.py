375d2f468c1ca9df31da0e9d863c969ccea0ed1a
emmacallahan <emma.callahan@scout24.com>
2019-09-16 21:10:00 +0200

    Cleaning the code

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,4 @@
 print("Let's play Tic-Tac-Toe!")
-# board_columns = '   1 2 3'
-# board_row_A = '\n A _|_|_'
-# board_row_B = '\n B _|_|_'
-# board_row_C = '\n C  | | '
 
 board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
 player_1 = 'X'
@@ -67,38 +63,6 @@ while no_winner() and is_there_a_free_space():
     board[row][column] = current_player
     print_board()
     current_player = player_1 if current_player == player_2 else player_2
-    
+
 current_player = player_1 if current_player == player_2 else player_2
 print(f'PLAYER {current_player} WON!')
-
-# def place_player_choice():
-#     player_input.lower()
-#     if player_input == ['A1','1A']:
-#         board_row_A == '\n A X|_|_'
-#
-# print_board()
-#
-# player_symbol = 'X'
-# player_input = ''
-#
-# def switch_player_symbol():
-#     if player_symbol == 'X':
-#         player_symbol == 'O'
-#         return player_symbol
-#     else:
-#         return player_symbol
-#
-# player_input = input('Player '+player_symbol+', please select a space:')
-#
-# print(player_input)
-#
-# switch_player_symbol()
-# print(player_input)
-# place_player_choice()
-# print_board()
-# print(player_input)
-#2D matirx with one position [[]]
-
-##def check_for_winning_combination():
-##    for char in board_vertical_A,board_vertical_B,board_vertical_C:
-##       if
